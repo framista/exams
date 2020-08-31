8370f10b3bfbe1b2163daeff7143b24d244fa526
framista <adajab1@gmail.com>
2020-08-31 15:24:59 +0200

    Setup basic routers

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppRouter from './routes/AppRouter';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
---
+++ src/routes/AppRouter.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import NotFoundPage from '../components/pages/not-found-page';
+import Home from '../components/pages/home';
+
+const AppRouter = () => {
+  return (
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
+    </Fragment>
+  );
+};
+
+export default AppRouter;

9cc042d1ea9bc270749fc6e5876b1cb239baac60
framista <adajab1@gmail.com>
2020-09-01 20:53:37 +0200

    Create modal with form to create or edit exam

--- src/components/layout/menu-home/index.js
+++ src/components/layout/menu-home/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { ButtonGroup, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 import { addFilter, clearFilters } from '../../../redux/exams/actions';
+import ModalForm from '../../layout/modal-form';
 
 const MenuHome = ({ filters, addFilter, clearFilters }) => {
+  const [modalFormShow, setModalFormShow] = useState(false);
   const handleFilter = (filter) => {
     if (filter === 'all') {
       return clearFilters();
@@ -13,32 +15,41 @@ const MenuHome = ({ filters, addFilter, clearFilters }) => {
     }
   };
   return (
-    <ButtonGroup className="mt-2 mb-4">
-      <Button variant="success">Nowy sprawdzian</Button>
-      <DropdownButton
-        variant="outline-success"
-        as={ButtonGroup}
-        title="Filtruj"
-        id="bg-nested-dropdown"
-        onSelect={(e) => handleFilter(e)}
-      >
-        <Dropdown.Item eventKey="all">Wszystko</Dropdown.Item>
-        <Dropdown.Item eventKey="future">Przyszłe</Dropdown.Item>
-        <Dropdown.Item eventKey="noResult">Nieocenione</Dropdown.Item>
-        <Dropdown.Item eventKey="failed">Niezaliczone</Dropdown.Item>
-        <Dropdown.Item eventKey="passed">Zaliczone</Dropdown.Item>
-      </DropdownButton>
-      <DropdownButton
-        variant="outline-success"
-        as={ButtonGroup}
-        title="Sortuj"
-        id="bg-nested-dropdown"
-        onSelect={(e) => console.log(e)}
-      >
-        <Dropdown.Item eventKey="date">Data</Dropdown.Item>
-        <Dropdown.Item eventKey="subject">Przedmiot</Dropdown.Item>
-      </DropdownButton>
-    </ButtonGroup>
+    <Fragment>
+      <ButtonGroup className="mt-2 mb-4">
+        <Button variant="success" onClick={() => setModalFormShow(true)}>
+          Nowy sprawdzian
+        </Button>
+        <DropdownButton
+          variant="outline-success"
+          as={ButtonGroup}
+          title="Filtruj"
+          id="bg-nested-dropdown"
+          onSelect={(e) => handleFilter(e)}
+        >
+          <Dropdown.Item eventKey="all">Wszystko</Dropdown.Item>
+          <Dropdown.Item eventKey="future">Przyszłe</Dropdown.Item>
+          <Dropdown.Item eventKey="noResult">Nieocenione</Dropdown.Item>
+          <Dropdown.Item eventKey="failed">Niezaliczone</Dropdown.Item>
+          <Dropdown.Item eventKey="passed">Zaliczone</Dropdown.Item>
+        </DropdownButton>
+        <DropdownButton
+          variant="outline-success"
+          as={ButtonGroup}
+          title="Sortuj"
+          id="bg-nested-dropdown"
+          onSelect={(e) => console.log(e)}
+        >
+          <Dropdown.Item eventKey="date">Data</Dropdown.Item>
+          <Dropdown.Item eventKey="subject">Przedmiot</Dropdown.Item>
+        </DropdownButton>
+      </ButtonGroup>
+      <ModalForm
+        show={modalFormShow}
+        onHide={() => setModalFormShow(false)}
+        title="Dodaj sprawdzian"
+      />
+    </Fragment>
   );
 };
 
